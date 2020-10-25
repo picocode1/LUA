3afbe3532fb307523d6c7603d73ba48cfdb5c8ff
picocode <38665815+picocode1@users.noreply.github.com>
2020-10-25 20:18:18 +0100

    Create [GAMESENSE] - LegitLUA.lua

---
+++ [GAMESENSE] - LegitLUA.lua
@@ -0,0 +1,20 @@
+local FOV = ui.reference('LEGIT', 'Aimbot', 'Maximum FOV')
+local Aimbot = ui.reference('LEGIT', 'Aimbot', 'Enabled')
+local x, y = client.screen_size()
+local FOV_Checkbox = ui.new_checkbox('LEGIT', 'Other', 'FOV Circle')
+local Color = ui.new_color_picker('LEGIT', 'Other', 'FOV Circle', 255, 255, 255, 255)
+local Aircheck_Checkbox = ui.new_checkbox('LEGIT', 'Other', 'Aircheck')
+
+function LegitFunction()
+    local r, g, b, a = ui.get(Color)
+    local Flags = entity.get_prop(entity.get_local_player(), 'm_fFlags')
+    if ui.get(FOV_Checkbox) == true then
+        renderer.circle_outline(x / 2, y / 2, r, g, b, a, ui.get(FOV), 1, 1, 1)
+    end
+    if (Flags == 257) and ui.get(Aircheck_Checkbox) == true then
+        ui.set(Aimbot, true)
+    elseif (Flags == 256) and ui.get(Aircheck_Checkbox) == true then
+        ui.set(Aimbot, false)
+    end
+end
+client.set_event_callback('paint', LegitFunction)
