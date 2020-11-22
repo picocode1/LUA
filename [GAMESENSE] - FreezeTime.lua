702ed268d201f713c03557c290bbb716e7ed1651
picocode <38665815+picocode1@users.noreply.github.com>
2020-11-22 17:35:11 +0100

    Create [GAMESENSE] - FreezeTime.lua

---
+++ [GAMESENSE] - FreezeTime.lua
@@ -0,0 +1,15 @@
+local freezetimer = ui.new_checkbox('AA', 'Other', "Freezetime Anti-Aim")
+function runFreezeTimeAA(cmd)
+    local pitch, yaw = client.camera_angles()
+    local bFreezePeriod = entity.get_prop(entity.get_game_rules(), "m_bFreezePeriod")
+    if ui.get(freezetimer) and (bFreezePeriod == 1) then
+        if (cmd.in_use == 1) then
+            --nothing
+        else
+            cmd.yaw = yaw + 180
+            cmd.pitch = 89
+        end
+    end
+end
+
+client.set_event_callback("setup_command", runFreezeTimeAA)
