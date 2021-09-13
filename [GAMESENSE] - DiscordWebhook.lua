a2fd878fded5ebc39b36bfa2899f46c0433e8596
picocode <38665815+picocode1@users.noreply.github.com>
2021-09-13 11:06:35 +0200

    Updated link

--- [GAMESENSE] - DiscordWebhook.lua
+++ [GAMESENSE] - DiscordWebhook.lua
@@ -1,14 +1,15 @@
 local message = ui.new_textbox("LUA", "a", "Send webhook", "Message")
+local webhook_link = "https://discordapp.com/api/webhooks/yourownlink"
 function sendMessage()
     local eval = [[
-        $.AsyncWebRequest("https://discordapp.com/api/webhooks/yourownlink", {
+        $.AsyncWebRequest("%s", {
             type: "POST",
             data: {
                 "content": "%s"
             }
         })
     ]]
-    print( panorama.loadstring( string.format( eval, ui.get(message) ) )() )
+    print( panorama.loadstring( string.format( eval, webhook_link, ui.get(message) ) )() )
     --message is content being send
 end
 local sendButton = ui.new_button("LUA", "a", "Send webhook", sendMessage)
