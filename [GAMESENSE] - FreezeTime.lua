457dc130e6bf117c7e65751189b6c61c372d8cfe
picocode <38665815+picocode1@users.noreply.github.com>
2021-10-12 11:31:02 +0200

    Update [GAMESENSE] - FreezeTime.lua

--- [GAMESENSE] - FreezeTime.lua
+++ [GAMESENSE] - FreezeTime.lua
@@ -3,13 +3,10 @@ function runFreezeTimeAA(cmd)
     local pitch, yaw = client.camera_angles()
     local bFreezePeriod = entity.get_prop(entity.get_game_rules(), "m_bFreezePeriod")
     if ui.get(freezetimer) and (bFreezePeriod == 1) then
-        if (cmd.in_use == 1) then
-            --nothing
-        else
+        if not cmd.in_use == 1 then
             cmd.yaw = yaw + 180
             cmd.pitch = 89
         end
     end
 end
-
 client.set_event_callback("setup_command", runFreezeTimeAA)
