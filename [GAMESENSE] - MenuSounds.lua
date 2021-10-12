62d33da72363e24f483dd8ef239aad677936c8d5
picocode <38665815+picocode1@users.noreply.github.com>
2021-10-12 11:39:29 +0200

    Improved code
    Not tested old code down below

--- [GAMESENSE] - MenuSounds.lua
+++ [GAMESENSE] - MenuSounds.lua
@@ -1,6 +1,24 @@
 --Download the files https://github.com/picocode1/LUA/blob/main/sound.zip
 local Sounds = {
     '-',
+    'menu1',
+    'menu2',
+    'menu3',
+    'menu4',
+    'menu5'
+}
+   
+local combo_box = ui.new_combobox('LUA', 'a', 'Select Sound', Sounds)
+client.set_event_callback("paint", function()
+        if ui.is_menu_open() == false and not ui.get(combo_box) == '-' then
+        client.exec(string.format("play %s.mp3", ui.get(combo_box)))
+    end
+end)
+
+
+--[[ backup
+local Sounds = {
+    '-',
     'Sound 1',
     'Sound 2',
     'Sound 3',
@@ -26,3 +44,4 @@ local Sounds = {
         end
     end
    client.set_event_callback("paint", MenuSound)
+]]
